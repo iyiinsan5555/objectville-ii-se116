5361cb10ce48657858bac0c9b935c9af8ad7ebc5
UtkanGobelek <gumututkan@gmail.com>
2026-05-05 18:32:54 +0300

    made location private

--- src/Cell.java
+++ src/Cell.java
@@ -1,7 +1,7 @@
 public abstract class Cell {
     private int x;
     private int y;
-    Point location;
+    private Point location;
     public Cell(int x, int y){
         location = new Point(x,y);
     }
