248ef19c9e43f74f338108474553d05351366d78
UtkanGobelek <gumututkan@gmail.com>
2026-05-19 18:30:53 +0300

    Changed the use of getNeighbors
    deleted the temporary built in method and started the use of GridManager's one

--- src/UtilityDistributor.java
+++ src/UtilityDistributor.java
@@ -26,38 +26,7 @@ public class UtilityDistributor{
             }
         }
     }
-
-    private Map<Point,Cell> grid = new HashMap<>();
-    
-
-    public ArrayList<Cell> getNeighbors(Cell start) {
-        ArrayList<Cell> neighbors = new ArrayList<>();
-
-        Point p = start.getLocation();
-        int x = p.getX();
-        int y = p.getY();
-
-        Cell north    = grid.get(new Point(x, y + 1));
-        Cell northEast = grid.get(new Point(x + 1,y + 1));
-        Cell east = grid.get(new Point(x + 1, y));
-        Cell southEast = grid.get(new Point(x + 1, y - 1));
-        Cell south = grid.get(new Point( x,y - 1));
-        Cell southWest = grid.get(new Point(x - 1,y - 1));
-        Cell west = grid.get(new Point(x - 1,y));
-        Cell northWest = grid.get(new Point( x - 1, y + 1));
-
-        if (north != null || north instanceof Transferable) neighbors.add(north);
-        if(northEast != null || northEast instanceof Transferable) neighbors.add(northEast);
-        if (east != null|| east instanceof Transferable) neighbors.add(east);
-        if(southEast != null || southEast instanceof Transferable) neighbors.add(southEast);
-        if (south != null || south instanceof Transferable) neighbors.add(south);
-        if(southWest != null|| southWest instanceof Transferable) neighbors.add(southWest);
-        if(west != null|| west instanceof Transferable) neighbors.add(west);
-        if(northWest != null|| northWest instanceof Transferable) neighbors.add(northWest);
-
-        return neighbors;
-    }
-
+        
     public void distribute() {
         distributeInternet();
         distributeWater();
@@ -87,7 +56,7 @@ public class UtilityDistributor{
                     startingCell.decTotalWater(consumed);
                 }
 
-                ArrayList<Cell> neighbors = getNeighbors(current);
+                ArrayList<Cell> neighbors = GridManager.getNeighbors(startingCell,cellHashMap);
                 //storing adjacent cells to the queue
                 for (Cell neighbor : neighbors) {
                     if (!visited.contains(neighbor)) {
@@ -129,7 +98,7 @@ public class UtilityDistributor{
 
 
 
-                ArrayList<Cell> neighbors = getNeighbors(current);
+                ArrayList<Cell> neighbors = GridManager.getNeighbors(startingCell,cellHashMap);
                 //storing adjacent cells to the queue
                 for (Cell neighbor : neighbors) {
                     if (!visited.contains(neighbor)) {
@@ -163,7 +132,7 @@ public class UtilityDistributor{
                     startingCell.decTotalElectric(consumed);
                 }
 
-                ArrayList<Cell> neighbors = getNeighbors(current);
+                ArrayList<Cell> neighbors = GridManager.getNeighbors(startingCell,cellHashMap);
                 //storing adjacent cells to the queue
                 for (Cell neighbor : neighbors) {
                     if (!visited.contains(neighbor)) {
